031eb6b6ad56131fe8a03e826ee68b35159efa2c
Mu-abdullah <moabdullah588@gmail.com>
2023-04-08 13:21:57 +0200

    create FeaturedBooks method

--- lib/features/home/presentation/manger/featured_books/featured_books_state.dart
+++ lib/features/home/presentation/manger/featured_books/featured_books_state.dart
@@ -8,3 +8,17 @@ abstract class FeaturedBooksState extends Equatable {
 }
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  const FeaturedBooksFailure(this.errMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  const FeaturedBooksSuccess(this.books);
+}
