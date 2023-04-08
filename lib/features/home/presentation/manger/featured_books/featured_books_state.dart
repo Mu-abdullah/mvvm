ef7d1ddee836140c197e9af003b96f140360b3db
Mu-abdullah <moabdullah588@gmail.com>
2023-04-08 13:08:56 +0200

    add bloc folders and classes

---
+++ lib/features/home/presentation/manger/featured_books/featured_books_state.dart
@@ -0,0 +1,10 @@
+part of 'featured_books_cubit.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
